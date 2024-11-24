d0a5d60e0c63d114fc2f6614be871a5cd23bdeb0
YeanSoo00 <kszm3434@gmail.com>
2024-11-24 09:53:05 +0900

    Task delete, sub-task add functionality added

--- ase_project/frontend/lib/components/task_card.dart
+++ ase_project/frontend/lib/components/task_card.dart
@@ -1,27 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:ase_project/models/task_model.dart';
 
+
 class TaskCard extends StatelessWidget {
   final Task task;
-  final VoidCallback onTap; // Pass a callback to handle the tap
+  final VoidCallback onTap; 
+  final VoidCallback onDelete; 
+  final VoidCallback onCreateSubTask; 
 
-  TaskCard({required this.task, required this.onTap});
+
+  const TaskCard({
+    super.key, 
+    required this.task,
+    required this.onTap,
+    required this.onDelete,
+    required this.onCreateSubTask,
+  });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap, // Trigger the onTap callback on tap
+      onTap: onTap,
       child: Card(
-        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(task.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              SizedBox(height: 8),
-              Text(task.description ?? "No description available", style: TextStyle(fontSize: 14)),
-              // Add any additional task details you want to display
+            children: [         
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        task.title,
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        task.description ?? "No description available",
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      //Sub-Task add 
+                      IconButton(
+                        icon: const Icon(Icons.add, color: Colors.blue),
+                        onPressed: onCreateSubTask, 
+                      ),
+                      // Task Delete
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: onDelete, 
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              
             ],
           ),
         ),
